7dae6f6effd8911a93b1c4e255d972cca5b0b74f
IbukiAlice <dxnbsxzy52048@126.com>
2020-06-18 17:09:18 +0800

    天赋树节点生成算法

--- src/swingtest/data/TalentNode.java
+++ src/swingtest/data/TalentNode.java
@@ -1,5 +1,7 @@
 package swingtest.data;
 
+import java.util.Random;
+
 /**
  * 天赋树节点
  */
@@ -27,6 +29,66 @@ public class TalentNode {
 	public TalentNode(int[] talent, int treeLevel){
 		isAvailable = false;
 
+		Random r = new Random();
+		effects = new int[8];
 
+		// 设置每种属性的概率
+		int[] effectPredict = new int[8];
+		final int[] pos = {0, 1, 1, 0, 2};
+
+		for(int i = 0; i < effectPredict.length; i++){
+			if(i < 3){
+				effectPredict[i] = talent[i] * 2;
+			}else{
+				effectPredict[i] = talent[pos[i - 3]];
+			}
+		}
+
+		effectPredict[6] += talent[2];
+
+		// 选择第一属性
+		int target = r.nextInt(1200);
+		int targetPos = 0;
+
+		for(int i = 0; i < effectPredict.length; i++){
+			target -= effectPredict[i];
+			if(target < 0){
+				targetPos = i;
+			}
+		}
+
+		// 设置属性值
+		effects[targetPos] = (targetPos == 6 ? 5 : 1) * (targetPos > 2 ? 2 : 1) * treeLevel;
+
+		// 设置升级速度
+		final int[] targetPosToLvSpeed = {0, 1, 2, 0, 1, 1, 0, 2};
+		if(targetPos == 6){
+			lvUpSpeed = Math.max(talent[0], talent[2]);
+		}else{
+			lvUpSpeed = talent[targetPosToLvSpeed[targetPos]];
+		}
+
+		// 第二属性是否开启
+		if(r.nextInt(10) == 0){
+
+			// 去掉第一属性
+			target = r.nextInt(1200 - effectPredict[targetPos]);
+			effectPredict[targetPos] = 0;
+
+			targetPos = 0;
+			for(int i = 0; i < effectPredict.length; i++){
+				target -= effectPredict[i];
+				if(target < 0){
+					targetPos = i;
+				}
+			}
+
+			// 设置属性值
+			effects[targetPos] = (targetPos == 6 ? 5 : 1) * (targetPos > 2 ? 2 : 1) * treeLevel / 3;
+		}
+
+		// 设置经验值相关
+		currentExp = 0;
+		totalExp = ((int)Math.pow(treeLevel, 1.5)) * 37 + 31;
 	}
 }
